f6afdf80d6cc40cf41a720f71ce24e014a905ba1
ysy <su12ng@gmail.com>
2026-05-27 09:13:54 +0900

    docs 디렉토리 클린

--- docs/notification-drop-fp100-fcm-token.sql
+++
@@ -1,8 +0,0 @@
--- fp_24 적재 경로 배포 후 실행
--- 전제:
--- 1. 로그인/소셜 로그인 요청에서 fcmToken, deviceId를 보내고 있어야 함
--- 2. 서버가 fp_24에 토큰을 upsert 하도록 배포되어 있어야 함
--- 3. PushNotificationService 가 fp_24만 사용하도록 반영되어 있어야 함
-
-alter table fp_100
-    drop column if exists fcm_token;
